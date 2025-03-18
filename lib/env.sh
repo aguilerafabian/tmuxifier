df2d541a28703b580c0d2c25d34f6329ee3375a4
Fabian Aguilera <aguilerafabian@gmail.com>
2025-03-18 16:34:27 -0300

    base_path para tmuxifier

--- lib/env.sh
+++ lib/env.sh
@@ -1,6 +1,13 @@
 # Setup layout path.
 if [ -z "${TMUXIFIER_LAYOUT_PATH}" ]; then
-  export TMUXIFIER_LAYOUT_PATH="${TMUXIFIER}/layouts"
+    export TMUXIFIER_LAYOUT_PATH="${TMUXIFIER}/layouts"
 else
-  export TMUXIFIER_LAYOUT_PATH="${TMUXIFIER_LAYOUT_PATH%/}"
+    export TMUXIFIER_LAYOUT_PATH="${TMUXIFIER_LAYOUT_PATH%/}"
+fi
+
+# Setup base path.
+if [ -z "${TMUXIFIER_SESSION_BASE_PATH}" ]; then
+    export TMUXIFIER_SESSION_BASE_PATH="${HOME}"
+else
+    export TMUXIFIER_SESSION_BASE_PATH="${TMUXIFIER_SESSION_BASE_PATH%/}"
 fi
--- templates/session.sh
+++ templates/session.sh
@@ -1,19 +1,19 @@
 # Set a custom session root path. Default is `$HOME`.
 # Must be called before `initialize_session`.
-#session_root "~/Projects/{{SESSION_NAME}}"
+#session_root "{{SESSION_PATH}}"
 
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
 if initialize_session "{{SESSION_NAME}}"; then
 
-  # Create a new window inline within session layout definition.
-  #new_window "misc"
+    # Create a new window inline within session layout definition.
+    #new_window "misc"
 
-  # Load a defined window layout.
-  #load_window "example"
+    # Load a defined window layout.
+    #load_window "example"
 
-  # Select the default active window on session creation.
-  #select_window 1
+    # Select the default active window on session creation.
+    #select_window 1
 
 fi
 
